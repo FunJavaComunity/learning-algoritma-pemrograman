020a5a2c1a36dfd8a5b0eeb9ebddb9f0f889e46b
Nilfgard13 <107827284+Nilfgard13@users.noreply.github.com>
2023-07-21 18:23:42 +0700

    Expression State Block

---
+++ src/ExpressionStatementBlock/ExpressionDkk.java
@@ -0,0 +1,26 @@
+package ExpressionStatementBlock;
+
+public class ExpressionDkk {
+    public static void main(String[] args) {
+
+        //Expression
+        int a;
+        a = 2;
+
+        //Statement
+        System.out.println(a);
+
+        //assignment statement
+        int angka = 76;
+        //increment statement
+        a++;
+        //method invocation statement
+        System.out.println("Hai");
+
+        //Block
+        {
+
+        }
+
+    }
+}
