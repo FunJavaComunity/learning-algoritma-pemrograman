62c4cc937f11037d72e8bea6a89141af1975bdbd
Nilfgard13 <107827284+Nilfgard13@users.noreply.github.com>
2023-07-23 16:00:50 +0700

    Create Augmented.java

---
+++ src/OperasiMatematika/Augmented.java
@@ -0,0 +1,20 @@
+package OperasiMatematika;
+
+public class Augmented {
+    public static void main(String[] args) {
+        //Augmented Assignments
+
+        int c = 20;
+
+        c += 10; //c = c + 10
+        System.out.println(c);
+        c -= 10; //c = c - 10
+        System.out.println(c);
+        c *= 10; //c = c * 10
+        System.out.println(c);
+        c /= 10; //c = c / 10
+        System.out.println(c);
+        c %= 10; //c = c % 10
+        System.out.println(c);
+    }
+}
