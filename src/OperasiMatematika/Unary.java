c2f95fbf9743b8b7c56cc1ebe808820ebe9a3263
Nilfgard13 <107827284+Nilfgard13@users.noreply.github.com>
2023-07-23 16:01:16 +0700

    Create Unary.java

---
+++ src/OperasiMatematika/Unary.java
@@ -0,0 +1,18 @@
+package OperasiMatematika;
+
+public class Unary {
+    public static void main(String[] args) {
+        //Unary Operator
+        int d = 50;
+
+
+        //Increment
+        d++;
+        d++;
+        System.out.println("Increment = " + d);
+
+        //decrement
+        d--;
+        System.out.println("Decrement = " + d);
+    }
+}
