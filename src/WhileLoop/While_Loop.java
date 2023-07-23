3de69c92847d69b428e5dd5291d7c5c6b3a6c62c
Nilfgard13 <107827284+Nilfgard13@users.noreply.github.com>
2023-07-23 16:04:31 +0700

    Update While_Loop.java

--- src/WhileLoop/While_Loop.java
+++ src/WhileLoop/While_Loop.java
@@ -1,31 +1,18 @@
 package WhileLoop;
 
-import java.util.Scanner;
-
 public class While_Loop {
     public static void main(String[] args) {
 
+//        while(condition){
+//
+//        }
+
         //Penggunaan sederhana while
-        int a = 0;
+        int a = 1;
 
-        while(a <= 50 ){
+        while(a <= 10){
             System.out.println("Looping - " + a);
-            a+=10;
-        }
-
-        //Contoh 2
-        while(true){
-
-            Scanner scan = new Scanner(System.in);
-            System.out.println("Apakah anda ingin berhenti?");
-            System.out.print("(y/t) ");
-
-            String input = scan.nextLine();
-
-            if(input.equalsIgnoreCase("y")){
-                break;
-            }
-
+            a++;
         }
 
     }
