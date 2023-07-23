7807d5b300dcb1b15ec3f70036ec4496212b5f7d
Nilfgard13 <107827284+Nilfgard13@users.noreply.github.com>
2023-07-23 15:56:45 +0700

    Update and rename ExpressionDkk.java to Expression.java

---
+++ src/ExpressionStatementBlock/Expression.java
@@ -0,0 +1,11 @@
+package ExpressionStatementBlock;
+
+public class Expression {
+    public static void main(String[] args) {
+
+        int b = 500 + 50;
+        int a;
+        int c = 4;
+        System.out.println(a = 3);
+    }
+}
